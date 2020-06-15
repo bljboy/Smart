35149ab95bdba9345e667b94708aef6e93ea68f5
bljboy <bljboy55@gmail.com>
2020-06-15 15:17:49 +0800

    path_bljboy

---
+++ app/src/main/java/com/example/path/MyView.java
@@ -0,0 +1,10 @@
+package com.example.path;
+
+import android.content.Context;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+}
